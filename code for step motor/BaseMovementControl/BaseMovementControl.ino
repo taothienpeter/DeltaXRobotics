3bbf9a1b971f6551d2282808d575abac2f3dfc9b
taothienpeter <93915439+taothienpeter@users.noreply.github.com>
2024-07-31 12:24:19 +0700

    reconfigure for adapting new ROS2 interface

--- code for step motor/BaseMovementControl/BaseMovementControl.ino
+++ code for step motor/BaseMovementControl/BaseMovementControl.ino
@@ -11,76 +11,20 @@
 AccelStepper LeftStepper(1, driverPUL_A, driverDIR_A); // (using driver mode, step pin, dir pin)
 AccelStepper RightStepper(1, driverPUL_B, driverDIR_B);
 // Variables
-
-class SimplePID{
-  private:
-    float kp, kd, ki, umax; // Parameters
-    float eprev, eintegral; // Storage
-
-  public:
-    // Constructor
-    SimplePID() : kp(1), kd(0), ki(0), umax(255), eprev(0.0), eintegral(0.0){}
-
-    // A function to set the parameters
-  void setParams(float kpIn, float kdIn, float kiIn, float umaxIn){
-    kp = kpIn; kd = kdIn; ki = kiIn; umax = umaxIn;
-  }
-
-  // A function to compute the control signal
-  void evalu(int value, int target, float deltaT, int &pwr, int &dir){
-    // error
-    int e = target - value;
-    // derivative
-    float dedt = (e-eprev)/(deltaT);
-    // integral
-    eintegral = eintegral + e*deltaT;
-    // control signal
-    float u = kp*e + kd*dedt + ki*eintegral;
-    // motor power
-    pwr = (int) fabs(u);
-    if( pwr > umax ){
-      pwr = umax;
-    }
-    // motor direction
-    dir = 1;
-    if(u<0){
-      dir = -1;
-    }
-    // store previous error
-    eprev = e;
-  }
-};
+float linear_x, angular_z; // tín hiệu điều khiển từ ROS2 jazzy
+float speed_linear, speed_angular; // tốc độ trung bình cho tịnh tiến, và rẽ
+int speed_A, speed_B; // tốc độ động cơ bước (tính bằng step/vòng)
 //some variable:
 float maxSpeed = 20000;
 float accceleration = 2000;
-
-String receivedData, stringPart, intPart1, intPart2;
-int pulse, time;
-
-int periodDelay = 500;       // Pulse Delay period (50-2000)
-boolean setdir = LOW; // Set Direction
-
-// Khai báo thời gian delay cho các xung PWM (điều chỉnh tốc độ)
-unsigned long delayTime1 = 500; // Thời gian delay cho động cơ 1 (micro giây)
-unsigned long delayTime2 = 300; // Thời gian delay cho động cơ 2 (micro giây)
-
-// Khai báo biến thời gian để theo dõi thời gian tạo xung PWM
-unsigned long previousMicros1 = 0;
-unsigned long previousMicros2 = 0;
-bool pulState1 = LOW;
-bool pulState2 = LOW;
-
-unsigned long looptime;
-void setup() {
-  Serial.begin(9600);
-
+void step_init(){
   LeftStepper.setMaxSpeed(maxSpeed);
   LeftStepper.setSpeed(0);
   LeftStepper.setAcceleration(accceleration);
   RightStepper.setMaxSpeed(maxSpeed);
   RightStepper.setSpeed(0);
   RightStepper.setAcceleration(accceleration);
-//================
+  //================
   pinMode (driverPUL_A, OUTPUT);
   pinMode (driverDIR_A, OUTPUT);
   pinMode (driverPUL_B, OUTPUT);
@@ -91,86 +35,34 @@ void setup() {
   digitalWrite(driverEnable_B, LOW);
   digitalWrite(driverDIR_A, LOW);
   digitalWrite(driverDIR_B, LOW);
-  /*
-  while (!Serial) {
-    ; // Đợi cho đến khi Serial kết nối
-  }
-  Serial.println("Serial communication started.");
-  Serial.println("Commands: <cmd> <pulse> <time>");
-  /*
-    cmd:forw, back, right, left
-    pulse: pulse/micro second
-    time: second 
-  */
 }
 
+void setup() {
+  Serial.begin(9600);
+  step_init();
+  
+}
 void loop() {    
-  serialRead();
-  //Serial.println("$");
-  /*
-//====================================
-      unsigned long startTime = millis();
-      unsigned long Rtime;
-    if(stringPart == "forw"){
-      while(Rtime < time){
-       Rtime = millis() - startTime;
-       setMotorSpeed(pulse, pulse);
-       setMotorDir(1);//1 = forward
-      }
-    }
-    if(stringPart == "back"){
-      while(Rtime < time){
-       Rtime = millis() - startTime;
-       setMotorSpeed(pulse, pulse);
-       setMotorDir(-1);//-1 = backward
-      }
-    }
-
-    
-    if(stringPart == "right"){
-      while(Rtime < time){
-       Rtime = millis() - startTime;
-       setMotorSpeed(pulse, pulse);
-       setMotorDir(2);//2 = right  <=============sửa thành right
-      }
-    }
-
-    if(stringPart == "left"){
-      while(Rtime < time){
-       Rtime = millis() - startTime;
-       setMotorSpeed(pulse, pulse);
-       setMotorDir(-2);//-2 = left  <=============sửa thành left
-      }
-    }    
-    */
+  serial_Read();
+  motor_Speed_Convert();
+  motor_Speed_Set();
 }
 
-//=============================================
-void serialRead(){
-    // read input
-    
-  if (Serial.available() > 0) {
-    receivedData = Serial.readStringUntil('\n'); // Đọc đến khi gặp ký tự xuống dòng
-    Serial.print("Received: ");
-    Serial.println(receivedData);
-
-    int firstSpaceIndex = receivedData.indexOf(' ');
-    int secondSpaceIndex = receivedData.indexOf(' ', firstSpaceIndex + 1);
-
-    if (firstSpaceIndex != -1 && secondSpaceIndex != -1) {
-      stringPart = receivedData.substring(0, firstSpaceIndex);
-      intPart1 = receivedData.substring(firstSpaceIndex + 1, secondSpaceIndex);
-      intPart2 = receivedData.substring(secondSpaceIndex + 1);
-
-      pulse = intPart1.toInt();
-      time = intPart2.toInt();
-      Serial.print(pulse); Serial.println(time);
-    }
-  }
+void serial_Read(){ // đọc thông tin gửi qua serial port
+   if (Serial.available() > 0) {// read input
+    String command = Serial.readStringUntil('\n');
+    linear_x = command.substring(0, command.indexOf(',')).toFloat();
+    angular_z = command.substring(command.indexOf(',') + 1).toFloat();}
 }
-void setMotorSpeed (int speed_A, int speed_B){ //pulse/micro second
+void motor_Speed_Convert(){ // tính toán vận tốc 2 động cơ
+    speed_A = (linear_x * speed_linear) - (angular_z * speed_angular);
+    speed_B = (linear_x * speed_linear) + (angular_z * speed_angular);
+}
+void motor_Speed_Set (){ //pulse/micro second
   RightStepper.setSpeed(speed_A);
   RightStepper.run();
   LeftStepper.setSpeed(speed_B);
   LeftStepper.run();
 }
+
+
