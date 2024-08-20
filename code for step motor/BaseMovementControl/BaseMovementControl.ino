de97bf8d0684c43b64d4beeec3304be9fede1ce1
taothienpeter <93915439+taothienpeter@users.noreply.github.com>
2024-08-20 09:35:18 +0700

    update

--- code for step motor/BaseMovementControl/BaseMovementControl.ino
+++ code for step motor/BaseMovementControl/BaseMovementControl.ino
@@ -1,21 +1,26 @@
 #include <util/atomic.h>
 #include <AccelStepper.h>
    // Push button for reverse on pin 2, pin 3
-#define driverPUL_A 7    // PUL- Apin
-#define driverDIR_A 6    // DIR- Apin
-#define driverPUL_B 8    // PUL- Bpin
-#define driverDIR_B 9    // DIR- Bpin
-#define driverEnable_A 10 
-#define driverEnable_B 11
+#define driverPUL_A 53    // PUL- Apin
+#define driverDIR_A 51    // DIR- Apin
+#define driverPUL_B 52    // PUL- Bpin
+#define driverDIR_B 50    // DIR- Bpin
+#define driverEnable_A 49 
+#define driverEnable_B 48
 
 AccelStepper LeftStepper(1, driverPUL_A, driverDIR_A); // (using driver mode, step pin, dir pin)
 AccelStepper RightStepper(1, driverPUL_B, driverDIR_B);
 // Variables
-float speed_linear, speed_angular; // tốc độ trung bình cho tịnh tiến, và rẽ
-int speed_L, speed_R; // tốc độ động cơ bước (tính bằng step/vòng)
+char argv1[16];
+char argv2[16];
+short index;
+
+float speed_L = 1600, speed_R = 1600; // tốc độ động cơ bước (tính bằng step/vòng)
+float* pSpeed_L = &speed_L;
+float* pSpeed_R = &speed_R;
 //some variable:
-float maxSpeed = 20000;
-float accceleration = 2000;
+float maxSpeed = 1600;
+float accceleration = 0;
 void step_init(){
   LeftStepper.setMaxSpeed(maxSpeed);
   LeftStepper.setSpeed(0);
@@ -43,20 +48,33 @@ void setup() {
 }
 void loop() {  
   serial_Read();
-  motor_Speed_Set();
+  //test();
 }
 
 void serial_Read(){ // đọc thông tin gửi qua serial port
-   if (Serial.available() > 0) {// read input
-    String command = Serial.readStringUntil('\n');
-    speed_L = command.substring(0, command.indexOf(',')).toFloat();
-    speed_R = command.substring(command.indexOf(',') + 1).toFloat();}
+
+  if (Serial.available() > 0) {// read input
+    
+    speed_L = Serial.readBytesUntil(',', char);
+    speed_R = Serial.parseInt();
+    Serial.println(speed_L);
+    Serial.println(speed_R);
+  }
+  else{
+    RightStepper.setSpeed(speed_L);
+    //Serial.print("ok");
+    RightStepper.run();
+    LeftStepper.setSpeed(speed_R);
+    LeftStepper.run();}
 }
-void motor_Speed_Set (){ //pulse/micro second
-  RightStepper.setSpeed(speed_L);
-  RightStepper.run();
-  LeftStepper.setSpeed(speed_R);
-  LeftStepper.run();
+void test(){
+    RightStepper.setSpeed(speed_L);
+    RightStepper.run();
+    LeftStepper.setSpeed(speed_R);
+    LeftStepper.run();
+}
+void feedback(){
+  Serial.write("OK");
 }
 
 
